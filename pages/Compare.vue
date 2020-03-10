4d63d95d495d18cf49ca1073c7ec25f7ec18f85a
Alex Gusev <alex@flancer64.com>
2020-03-10 17:04:25 +0200

    VSFSUPRU-16 Missed attributes names in Products comparison

--- pages/Compare.vue
+++ pages/Compare.vue
@@ -2,7 +2,7 @@
   <div class="compare">
     <div class="bg-cl-secondary py35 pl20">
       <div class="container">
-        <breadcrumbs :with-homepage="true" :routes="[]" active-route="Compare" />
+        <breadcrumbs :with-homepage="true" :routes="[]" active-route="Compare"/>
         <h2>{{ title }}</h2>
       </div>
     </div>
@@ -16,11 +16,11 @@
               </div>
               <ul class="compare__features-list">
                 <li
-                  v-for="(attr, index) in all_comparable_attributes"
+                  v-for="(attr, index) in available_comparable_attributes"
                   :key="index"
                   class="compare__features-item"
                 >
-                  {{ attr.default_frontend_label }}
+                  {{ attr.frontend_label }}
                 </li>
               </ul>
             </div>
@@ -33,12 +33,12 @@
                   data-testid="comparedProduct"
                 >
                   <div class="compare__top-info">
-                    <div class="check" />
-                    <product-tile class="col-md-12 collection-product" :product="product" />
+                    <div class="check"/>
+                    <product-tile class="col-md-12 collection-product" :product="product"/>
                   </div>
                   <ul class="compare__features-list">
                     <li
-                      v-for="(attr, attIndex) in all_comparable_attributes"
+                      v-for="(attr, attIndex) in available_comparable_attributes"
                       :key="attIndex"
                       class="compare__features-item"
                     >
@@ -89,11 +89,27 @@ export default {
       required: true
     }
   },
-  metaInfo () {
+  computed: {
+    available_comparable_attributes: function () {
+      let result = [];
+      for (let attr of this.all_comparable_attributes) {
+        if (attr.is_comparable) {
+          const code = attr.attribute_code;
+          let add_attr_to_result = false;
+          for (let item of this.items) {
+            if (item[code]) result.push(attr);
+            break;
+          }
+        }
+      }
+      return result;
+    }
+  },
+  metaInfo() {
     return {
       title: this.$route.meta.title || this.title || i18n.t('Compare Products'),
       meta: this.$route.meta.description
-        ? [{ vmid: 'description', description: this.$route.meta.description }]
+        ? [{vmid: 'description', description: this.$route.meta.description}]
         : []
     }
   }
@@ -101,149 +117,149 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '~theme/css/variables/colors';
-@import '~theme/css/helpers/functions/color';
+  @import '~theme/css/variables/colors';
+  @import '~theme/css/helpers/functions/color';
 
-$color-white: color(white);
-$border: mix(#000, $color-white, 10%);
-$color-product-bg: color(secondary, $colors-background);
+  $color-white: color(white);
+  $border: mix(#000, $color-white, 10%);
+  $color-product-bg: color(secondary, $colors-background);
 
-$products-column-width-mobile: 140px;
-$products-column-width: 280px;
-$features-column-width-mobile: 110px;
-$features-column-width: 210px;
+  $products-column-width-mobile: 140px;
+  $products-column-width: 280px;
+  $features-column-width-mobile: 110px;
+  $features-column-width: 210px;
 
-$screen-l: 1170px;
+  $screen-l: 1170px;
 
-*,
-*::after,
-*::before {
-  box-sizing: border-box;
-}
-
-.compare {
-  &__products-wrapper {
-    overflow-x: auto;
-    -webkit-overflow-scrolling: touch;
-    border-style: solid;
-    border-color: $border;
-    border-width: 1px 0 1px 0;
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
   }
 
-  &__products-columns {
-    margin: 0 0 0 $features-column-width-mobile;
-    padding: 0;
-    display: flex;
-
-    @media (min-width: $screen-l) {
-      margin-left: $features-column-width;
-    }
-  }
-
-  &__products-table {
-    position: relative;
-    overflow: hidden;
-  }
-
-  &__features {
-    position: absolute;
-    z-index: 1;
-    top: 0;
-    left: 0;
-    width: $features-column-width-mobile;
-    border-style: solid;
-    border-color: $border;
-    border-width: 1px 0 1px 0;
-    background-color: mix(#000, $color-white, 2%);
-    opacity: 0.95;
-
-    @media (min-width: $screen-l) {
-      width: $features-column-width;
-    }
-  }
-
-  &__product {
-    list-style: none;
-    position: relative;
-    float: left;
-    width: $products-column-width-mobile;
-    text-align: center;
-    transition: opacity 0.3s, visibility 0.3s, transform 0.3s;
-
-    @media (min-width: $screen-l) {
-      width: $products-column-width;
-    }
-  }
-
-  &__attributes {
-    font-weight: bold;
-    font-size: 0.6rem;
-    line-height: 14px;
-    padding: 15px 5px;
-    text-align: left;
-
-    @media (min-width: $screen-l) {
-      padding: 25px 10px;
-      line-height: 16px;
-      font-size: 0.7rem;
-    }
-  }
-
-  &__top-info {
-    position: relative;
-    height: 250px;
-    width: $products-column-width-mobile;
-    text-align: center;
-    border-color: $border;
-    border-style: solid;
-    border-width: 0 1px 0 0;
-    transition: height 0.3s;
-    cursor: pointer;
-    background: $color-product-bg;
-    overflow: hidden;
-
-    @media (min-width: $screen-l) {
-      height: 385px;
-      width: $products-column-width;
+  .compare {
+    &__products-wrapper {
+      overflow-x: auto;
+      -webkit-overflow-scrolling: touch;
+      border-style: solid;
+      border-color: $border;
+      border-width: 1px 0 1px 0;
     }
 
-    .compare__features & {
-      @extend .compare__attributes;
+    &__products-columns {
+      margin: 0 0 0 $features-column-width-mobile;
+      padding: 0;
+      display: flex;
+
+      @media (min-width: $screen-l) {
+        margin-left: $features-column-width;
+      }
+    }
+
+    &__products-table {
+      position: relative;
+      overflow: hidden;
+    }
+
+    &__features {
+      position: absolute;
+      z-index: 1;
+      top: 0;
+      left: 0;
       width: $features-column-width-mobile;
-      cursor: auto;
-      background: mix(#000, $color-white, 2%);
+      border-style: solid;
+      border-color: $border;
+      border-width: 1px 0 1px 0;
+      background-color: mix(#000, $color-white, 2%);
+      opacity: 0.95;
 
       @media (min-width: $screen-l) {
         width: $features-column-width;
       }
     }
-  }
 
-  &__features-list {
-    margin: 0;
-    padding: 0;
-  }
-
-  &__features-item {
-    @extend .compare__attributes;
-    border-color: $border;
-    border-style: solid;
-    border-width: 1px 1px 0 0;
-    list-style: none;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
-
-    .compare__products-columns & {
-      font-weight: normal;
+    &__product {
+      list-style: none;
+      position: relative;
+      float: left;
+      width: $products-column-width-mobile;
       text-align: center;
+      transition: opacity 0.3s, visibility 0.3s, transform 0.3s;
+
+      @media (min-width: $screen-l) {
+        width: $products-column-width;
+      }
+    }
+
+    &__attributes {
+      font-weight: bold;
+      font-size: 0.6rem;
+      line-height: 14px;
+      padding: 15px 5px;
+      text-align: left;
+
+      @media (min-width: $screen-l) {
+        padding: 25px 10px;
+        line-height: 16px;
+        font-size: 0.7rem;
+      }
+    }
+
+    &__top-info {
+      position: relative;
+      height: 250px;
+      width: $products-column-width-mobile;
+      text-align: center;
+      border-color: $border;
+      border-style: solid;
+      border-width: 0 1px 0 0;
+      transition: height 0.3s;
+      cursor: pointer;
+      background: $color-product-bg;
+      overflow: hidden;
+
+      @media (min-width: $screen-l) {
+        height: 385px;
+        width: $products-column-width;
+      }
+
+      .compare__features & {
+        @extend .compare__attributes;
+        width: $features-column-width-mobile;
+        cursor: auto;
+        background: mix(#000, $color-white, 2%);
+
+        @media (min-width: $screen-l) {
+          width: $features-column-width;
+        }
+      }
+    }
+
+    &__features-list {
+      margin: 0;
+      padding: 0;
+    }
+
+    &__features-item {
+      @extend .compare__attributes;
+      border-color: $border;
+      border-style: solid;
+      border-width: 1px 1px 0 0;
+      list-style: none;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
+
+      .compare__products-columns & {
+        font-weight: normal;
+        text-align: center;
+      }
+    }
+
+    &__remove {
+      position: absolute;
+      top: 0;
+      right: 0;
     }
   }
-
-  &__remove {
-    position: absolute;
-    top: 0;
-    right: 0;
-  }
-}
 </style>
